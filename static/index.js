5578ed60dcd891cca04c0cc520527f93a7088fcd
Salmancode76 <96613692+Salmancode76@users.noreply.github.com>
2025-01-31 18:44:52 +0300

    v2.0

--- static/index.js
+++ static/index.js
@@ -5,12 +5,37 @@ import {login} from "./Handlers/login.js"
 
 handleInitialLoad();
 
+document.getElementById('hamICON').addEventListener('click', function() {
+    const links = document.querySelector('.nav-links');
+    links.classList.toggle('active');
+});
+
 function handleInitialLoad() {
     const path = window.location.pathname;
     console.log(path)
     navigateTo(path);
 }
 async function navigateTo(route) {
+    const nav = document.getElementById('nav');
+
+    nav.innerHTML = `
+    <nav>
+        <ul>
+            <li class="header">  <a href="/" onclick="navigateTo('/'); return false;">  Community Forum </a> </li>
+            <div class="nav-links">
+                <li> <a href="/" onclick="navigateTo('/'); return false;"> Posts</a></li>
+
+                <li><a href="#">All users</a></li>
+                <li><a href="#">Private Messages</a></li>
+                <li><a href="#">Welcome, Stranger</a></li>
+                <li><a href="#">Logout</a></li>
+            </div>
+            <img id="hamICON" src="/static/icons8-hamburger.svg" alt="Menu">
+        </ul>
+    </nav>
+    
+    
+    `
     const app = document.getElementById('app');
 
     try {
